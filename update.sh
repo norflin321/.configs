e10bf6620a35bf16a60c4d74b2ee2b93995f29b1
Anton Kurtin <norflin321@gmail.com>
2025-04-10 04:19:28 +0300

    update

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ git rm --ignore-unmatch -rf ./nvim
 git rm --ignore-unmatch -rf ./ghostty
 git rm --ignore-unmatch -rf repoavg.sh
 git rm --ignore-unmatch -rf ./zed/settings.json
-git rm --ignore-unmatch -rf ./zed/keymaps.json
+git rm --ignore-unmatch -rf ./zed/keymap.json
 git rm --ignore-unmatch -rf ./zed/themes
 
 cp ~/.zshrc ./
@@ -15,7 +15,7 @@ cp -R ~/.config/nvim ./
 cp -R ~/.config/ghostty ./
 cp ~/.config/repoavg.sh ./
 cp -R ~/.config/zed/settings.json ./zed/
-cp -R ~/.config/zed/keymaps.json ./zed/
+cp -R ~/.config/zed/keymap.json ./zed/
 cp -R ~/.config/zed/themes ./zed/themes
 
 git add . && git commit -m "update" && git push
