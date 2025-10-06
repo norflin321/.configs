8dbcb6c058c6c03539635757c64fd87b87478c65
Anton Kurtin <norflin321@gmail.com>
2025-10-06 20:24:04 +0300

    ...

--- nvim/colors.lua
+++ nvim/colors.lua
@@ -1,6 +1,67 @@
 vim.g.colors_name = "colors"
 
--- colors.lua
+function hex_to_hsl(hex)
+	hex = hex:gsub("#", "")
+	local r = tonumber(hex:sub(1, 2), 16) / 255
+	local g = tonumber(hex:sub(3, 4), 16) / 255
+	local b = tonumber(hex:sub(5, 6), 16) / 255
+	local max = math.max(r, g, b)
+	local min = math.min(r, g, b)
+	local h, s, l = 0, 0, (max + min) / 2
+	if max == min then
+		h, s = 0, 0
+	else
+		local d = max - min
+		s = l > 0.5 and d / (2 - max - min) or d / (max + min)
+		if max == r then
+			h = (g - b) / d + (g < b and 6 or 0)
+		elseif max == g then
+			h = (b - r) / d + 2
+		elseif max == b then
+			h = (r - g) / d + 4
+		end
+		h = h / 6
+	end
+	h = math.floor(h * 360 + 0.5)
+	s = math.floor(s * 100 + 0.5)
+	l = math.floor(l * 100 + 0.5)
+	return h, s, l
+end
+
+function hsl_to_hex(h, s, l)
+	h = h/360
+	s = s/100
+	l = l/100
+	local r, g, b
+	if s == 0 then
+		r, g, b = l, l, l
+	else
+		local function hue_to_rgb(p, q, t)
+			if t < 0 then t = t + 1 end
+			if t > 1 then t = t - 1 end
+			if t < 1/6 then return p + (q - p) * 6 * t end
+			if t < 1/2 then return q end
+			if t < 2/3 then return p + (q - p) * (2/3 - t) * 6 end
+			return p
+		end
+		local q = l < 0.5 and l * (1 + s) or l + s - l * s
+		local p = 2 * l - q
+		r = hue_to_rgb(p, q, h + 1/3)
+		g = hue_to_rgb(p, q, h)
+		b = hue_to_rgb(p, q, h - 1/3)
+	end
+	r = math.floor(r * 255 + 0.5)
+	g = math.floor(g * 255 + 0.5)
+	b = math.floor(b * 255 + 0.5)
+	return string.format("#%02X%02X%02X", r, g, b)
+end
+
+function hex_add_lightness(hex, lightness)
+	local h, s, l = hex_to_hsl(hex)
+	hex = hsl_to_hex(h, s, l + lightness)
+	return hex
+end
+
 local colors = {
 	fg_normal = "#9ea3c0",
 	bg_normal = "#252737",
